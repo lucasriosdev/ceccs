2abddde86a5767e99cef882bf52776b4d3b8ee86
Lucas Rios <lucasriosdev@gmail.com>
2025-04-22 10:15:50 -0300

    alteração no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
@@ -8,17 +8,22 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-4">Endereço</h3>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-4">
+              Endereço
+            </h3>
             <p className="text-gray-600 dark:text-gray-300">
-              Av. 2 de Julho, 737, Casa Centro<br />
-              44620-000 Baixa Grande/BA
+              R. Pôrto Seguro, 1030 - Santa Cruz, <br /> Luís Eduardo Magalhães
+              - BA, 47850-000
             </p>
           </div>
           <div>
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-4">Horário de Atendimento</h3>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-4">
+              Horário de Atendimento
+            </h3>
             <p className="text-gray-600 dark:text-gray-300">
-              Segunda a Quinta: 08:00 às 12:00 e 14:00 às 17:00<br />
-              Sexta: 08:00 às 13:00
+              Segunda a Sexta: 07:30 às 11:30 e 13:30 <br /> às 17:30 e 19:00 às
+              22:30
+              <br />
             </p>
           </div>
         </div>
